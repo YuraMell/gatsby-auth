a461ed5a49e421fa9c3c3776fe09d6c1d4fb5d42
YuraMell <yura.melnik7523@gmail.com>
2022-04-20 15:02:40 +0300

    add form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState, useRef } from "react"
 
 import { navigate } from "gatsby"
 import netlifyIdentity from 'netlify-identity-widget'
@@ -10,6 +10,9 @@ import * as styles from '../styles/Login.module.css'
 
 
 const Login = () => {
+  const [value, setValue] = useState('')
+  const formRef = useRef(null)
+
   useEffect(() => {
     netlifyIdentity.init({})
   })
@@ -17,6 +20,34 @@ const Login = () => {
   netlifyIdentity.on('close', (_) => {
     navigate('/')
   })
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) =>
+          encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  const handleChange = e => setValue(e.target.value)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let formData = new FormData(formRef.current);
+    console.log(formData)
+
+    let response = await fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: encode({
+        "pizzaOrder": event.target.getAttribute("name"),
+      }),
+    })
+
+    let result = response.json()
+    console.log(result)
+  };
 
   return (
     <Layout>
@@ -35,6 +66,17 @@ const Login = () => {
           Sign up
         </span>
       </div>
+      <form
+        name="contact-form"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input name="name" placeholder="Your Name" type="text" />
+        <input name="email" placeholder="name@name.com" type="email" />
+        <input name="phone" type="text" />
+        <button>Send</button>
+      </form>
       <CustomLink to="/" value='Go back to the homepage' />
     </Layout>
   )
